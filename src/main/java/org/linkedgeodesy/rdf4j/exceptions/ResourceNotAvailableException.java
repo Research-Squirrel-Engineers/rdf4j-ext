fc779a93fcc7164f31a3f1f260cf653b64cafe35
Florian Thiery <web@florian-thiery.de>
2018-02-27 13:31:36 +0100

    add exception

---
+++ src/main/java/org/linkedgeodesy/rdf4j/exceptions/ResourceNotAvailableException.java
@@ -0,0 +1,12 @@
+package org.linkedgeodesy.rdf4j.exceptions;
+
+public class ResourceNotAvailableException extends Exception {
+
+	public ResourceNotAvailableException(String message) {
+		super(message);
+	}
+	
+	public ResourceNotAvailableException() {
+		super();
+	}
+}
